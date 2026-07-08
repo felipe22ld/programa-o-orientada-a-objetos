310105dbb7b37c9c3558a377325d43ec75d31b25
Felipe <felipe@techdog.com.br>
2026-07-08 15:03:38 -0300

    propriedade somente leitura

--- POO/ClassesEOutrosTipos/Cadastro/Produto.cs
+++ POO/ClassesEOutrosTipos/Cadastro/Produto.cs
@@ -2,5 +2,64 @@ namespace POO.ClassesEOutrosTipos.Cadastro;
 
 public class Produto
 {
-    
+    private int Id;
+
+    // isso é uma propriedade
+    public string Descricao { get; set; }
+
+    //Comportamento da classe => método
+    public void ImprimirDescricao()
+    {
+        //Ação que será executada
+        Console.WriteLine(Descricao);
+    }
+
+    public void SetId(int id)
+    {
+        Id = id;
+    }
+
+    public int GetId()
+    {
+        return Id;
+    }
+
+    public static void Executar()
+    {
+//classe dinâmica, instância o obj e utiliza o método
+        var produto = new Produto
+        {
+            Descricao = "Teclado"
+        };
+        produto.SetId(1);
+        produto.ImprimirDescricao();
+        Console.WriteLine(produto.GetId());
+        produto.ImprimirDescricao();
+
+//classe estatica, chama o método direto da classe. Não precisa instanciar o obj
+        var resultado = Cadastro.Calculos.SomarNumeros(2, 3);
+        Console.WriteLine(resultado);
+
+// classe => Modelo para representar um objeto no mundo real.
+//objeto => é um instancia de uma classe cria com o new.
+//propriedades => guarda informações sobre o obj.
+// metodo => executa a ação do objeto.
+//public => pode ser acessado de qualquer lugar da aplicação.
+//praivado => ´so pode ser acessado dentro da própria classe.
+//classe estática => não precisa ser instanciada, só pode ser acessada diretamente pelo nome da classe.
+    }
+}
+
+public static class Calculos
+{
+    public static int SomarNumeros(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void Executar()
+    {
+        var resultado = Calculos.SomarNumeros(2, 3);
+        Console.WriteLine(resultado);
+    }
 }
--- POO/Program.cs
+++ POO/Program.cs
@@ -1,14 +1,6 @@
-namespace ProjetoAula
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            // Financeiro.Funcionario funcionario = new Financeiro.Funcionario();
-            // Financeiro.ContasReceber contasReceber = new Financeiro.ContasReceber();
-            // Cadastro.Cliente cadastro = new Cadastro.Cliente();
-            // Cadastro.Funcionario funcionario1 = new Cadastro.Funcionario();
-        }
-    }
-};
+// using POO.ClassesEOutrosTipos.Cadastro;
+
+// Produto.Executar();
+// Calculos.Executar();
+
+Console.WriteLine("oi");
